a75739a8a6bcd7f74228904c681268528c32e2b4
Tomasz Mloduchowski <q@qdot.me>
2014-09-12 15:12:26 +0200

    My manual changes

--- www/js/index.js
+++ www/js/index.js
@@ -43,7 +43,42 @@ var app = {
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
+    
+        var pushNotification = window.plugins.pushNotification;
+        pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"847644074324","ecb":"app.onNotificationGCM"});
+
 
         console.log('Received Event: ' + id);
+    },
+    successHandler: function(result) {
+        alert('Callback Success! Result = '+result)
+    },
+    errorHandler:function(error) {
+        alert(error);
+    },
+    onNotificationGCM: function(e) {
+        switch( e.event )
+        {
+            case 'registered':
+                if ( e.regid.length > 0 )
+                {
+                    console.log("Regid " + e.regid);
+                    alert('registration id = '+e.regid);
+                }
+            break;
+
+            case 'message':
+                // this is the actual push notification. its format depends on the data model from the push server
+                alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+            break;
+
+            case 'error':
+                alert('GCM error = '+e.msg);
+            break;
+
+            default:
+                alert('An unknown GCM event has occurred');
+            break;
+        }
     }
 };
