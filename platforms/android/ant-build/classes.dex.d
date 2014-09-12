5e4e6e72e07dc520ef97ddea13ac612696d6edc3
Tomasz Mloduchowski <q@qdot.me>
2014-09-12 15:18:00 +0200

    deleting files that have been modified here

--- platforms/android/ant-build/classes.dex.d
+++
@@ -1,9 +0,0 @@
-/home/qdot/peerj/PeerJ_Push/platforms/android/ant-build/classes.dex : \
-/home/qdot/peerj/PeerJ_Push/platforms/android/ant-build/classes/com/peerj/n/PeerJPush.class \
-/home/qdot/peerj/PeerJ_Push/platforms/android/ant-build/classes/com/peerj/n/R$string.class \
-/home/qdot/peerj/PeerJ_Push/platforms/android/ant-build/classes/com/peerj/n/R$xml.class \
-/home/qdot/peerj/PeerJ_Push/platforms/android/ant-build/classes/com/peerj/n/BuildConfig.class \
-/home/qdot/peerj/PeerJ_Push/platforms/android/ant-build/classes/com/peerj/n/R.class \
-/home/qdot/peerj/PeerJ_Push/platforms/android/ant-build/classes/com/peerj/n/R$attr.class \
-/home/qdot/peerj/PeerJ_Push/platforms/android/ant-build/classes/com/peerj/n/R$drawable.class \
-/home/qdot/peerj/PeerJ_Push/platforms/android/ant-build/dexedLibs/classes-5a8744ace5d55c06727c41f323aa51f3.jar \
